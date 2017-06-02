edd96d14eb5a7b91dae4380deee433e15218e865
Akshat Gupta <akshat41121995@gmail.com>
2017-06-02 19:22:32 +0530

    Change in admission view

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -8,8 +8,11 @@
 class Dashboard extends CI_Controller{
     public function index(){
         
-        $this->load->view('dashboard/examples/dashboard');
-        
+        $this->load->view('dashboard/examples/dashboard_view');
+
+    }
+    public function admission_view(){
+    	$this->load->view('admission_view');
     }
     public function logout(){
         session_destroy();
