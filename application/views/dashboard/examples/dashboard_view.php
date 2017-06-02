e670421fabfa295265441515166450923f8e372c
ri-dhimagarg1 <garg.ridhima72@gmail.com>
2017-06-03 00:21:14 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ application/views/dashboard/examples/dashboard_view.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php base_url();?>public/assets/img/apple-icon.png" />
+	<link rel="icon" type="image/png" href="<?php base_url();?>public/assets/img/favicon.png" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>Dashboard</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Bootstrap core CSS     -->
+    <link href="<?php base_url();?>public/assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!--  Material Dashboard CSS    -->
+    <link href="<?php base_url();?>public/assets/css/material-dashboard.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="<?php base_url();?>public/assets/css/demo.css" rel="stylesheet" />
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+	<div class="wrapper">
+
+	    <div class="sidebar" data-color="purple" data-image="../assets/img/sidebar-1.jpg">
+			<!--
+		        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+		        Tip 2: you can also add an image using data-image tag
+		    -->
+			<div class="logo">
+				<a href="http://www.futureheadtech.com" class="simple-text">
+					Dashboard
+				</a>
+			</div>
+
+	    	<div class="sidebar-wrapper">
+	            <ul class="nav">
+	                <li class="active">
+	                    <a href="<?php site_url('admission_view'); ?>">
+	                        <i class="material-icons">dashboard</i>
+	                        <p>Admission</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="icons.php">
+	                        <i class="material-icons">person</i>
+	                        <p>Function 2</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="table.html">
+	                        <i class="material-icons">content_paste</i>
+	                        <p>Function 3</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="typography.html">
+	                        <i class="material-icons">library_books</i>
+	                        <p>Function 4</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="icons.html">
+	                        <i class="material-icons">bubble_chart</i>
+	                        <p>Function 5</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="maps.html">
+	                        <i class="material-icons">location_on</i>
+	                        <p>Function 6</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="notifications.html">
+	                        <i class="material-icons text-gray">notifications</i>
+	                        	<p>Function 7</p>
+	                    </a>
+	                </li>
+					<li class="active-pro">
+	                    <a href="upgrade.html">
+	                        <i class="material-icons">unarchive</i>
+	                        <p>Function 8</p>
+	                    </a>
+	                </li>
+	            </ul>
+	    	</div>
+	    </div>
+
+	    <div class="main-panel">
+			<nav class="navbar navbar-transparent navbar-absolute">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Admission Dashboard</a>
+					</div>
+					
+
+
+			
+					
+				
+		</div>
+	</div>
+
+</body>
+
+	<!--   Core JS Files   -->
+	<script src="../assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
+	<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="../assets/js/material.min.js" type="text/javascript"></script>
+
+	<!--  Charts Plugin -->
+	<script src="<?php base_url();?>public/assets/js/chartist.min.js"></script>
+
+	<!--  Notifications Plugin    -->
+	<script src="<?php base_url();?>public/assets/js/bootstrap-notify.js"></script>
+
+	<!--  Google Maps Plugin    -->
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+	<!-- Material Dashboard javascript methods -->
+	<script src="<?php base_url();?>public/assets/js/material-dashboard.js"></script>
+
+	<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+	<script src="<?php base_url();?>public/assets/js/demo.js"></script>
+
+	<script type="text/javascript">
+    	$(document).ready(function(){
+
+			// Javascript method's body can be found in assets/js/demos.js
+        	demo.initDashboardPageCharts();
+
+    	});
+	</script>
+
+</html>
