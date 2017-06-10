f7f55ee58b0d33c9953edb41f021ba9dec3a7055
Ridhima Garg <garg.ridhima72@gmail.com>
2017-06-10 12:23:03 -0700

    changes in employee view
    changes in employee view

--- application/controllers/employees.php
+++ application/controllers/employees.php
@@ -7,7 +7,7 @@
  */
 class Employees extends CI_Controller{
     public function index(){
-
+    	$this->load->view('dashboard/examples/employees_view');
     }
     public function employees_view(){
         $this->load->view('dashboard/examples/employees_view');
--- application/views/dashboard/examples/employees_view.php
+++ application/views/dashboard/examples/employees_view.php
@@ -25,39 +25,33 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Employees</a>
+            <a class="navbar-brand"  href="#">Employees</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
+            <ul class="nav navbar-nav navbar-right">
+                 <form class="navbar-form navbar-left" role="search">
                 <div class="form-group">
                     <input type="text" class="form-control" placeholder="Search">
                 </div>
                 <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="">Logout</a></li>
+                 </form>
             </ul>
         </div>
     </div>
 </nav>
-</body>
+<div class="row">
+    <div class="col-sm-8">
+    <button type="button" class="btn">Create</button>
+    </div>
+    <div class="col-sm-4" >
+     <div class="btn-group">
+  <button type="button" class="btn btn-primary">Apple</button>
+  <button type="button" class="btn btn-primary">Samsung</button>
+  <button type="button" class="btn btn-primary">Sony</button>
+</div> 
+    </div>
+</div>
 <script src="<?= base_url()?>public/assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
 <script src="<?= base_url()?>public//assets/js/bootstrap.min.js" type="text/javascript"></script>
 </html>
