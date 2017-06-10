46e1d29b0cbcc3bd5a9ed1dbba65763015c0536f
Akshat Gupta <akshat41121995@gmail.com>
2017-06-10 22:55:31 +0530

    Changes in link on navbar brand

--- application/views/dashboard/examples/accounting_view.php
+++ application/views/dashboard/examples/accounting_view.php
@@ -23,7 +23,8 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Accounting</a>
+            <div class="navbar-brand">Accounting</div>
+
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- application/views/dashboard/examples/students_view.php
+++ application/views/dashboard/examples/students_view.php
@@ -23,7 +23,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Students</a>
+            <div class="navbar-brand">Students</div>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
