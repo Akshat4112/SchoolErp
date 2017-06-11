ea7260392cd2fdd4c859c58bacad4529207f4441
Ridhima Garg <garg.ridhima72@gmail.com>
2017-06-11 12:01:27 -0700

    changes in employee
    changes in employee_view

--- application/views/dashboard/examples/employees_view.php
+++ application/views/dashboard/examples/employees_view.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width" />
 
     <link href="<?= base_url()?>public/assets/css/bootswatch_paper.css" rel="stylesheet" />
-
+    <!--link href="<?= base_url()?>public/assets/css/bootstrap.min.css" rel="stylesheet" /-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
     <link href="<?=base_url()?>public/assets/css/font-awesome.min.css" rel="stylesheet">
     <link href='<?=base_url()?>public/assets/css/google_fonts.css' rel='stylesheet' type='text/css'>
     <title>
@@ -34,7 +35,7 @@
                 <div class="form-group">
                     <input type="text" class="form-control" placeholder="Search">
                 </div>
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-default">  Submit</button>
                  </form>
             </ul>
         </div>
@@ -42,15 +43,52 @@
 </nav>
 <div class="row">
     <div class="col-sm-8">
-    <button type="button" class="btn">Create</button>
+    <button type="button" class="btn" style="margin-left: 12px;">Create</button>
     </div>
     <div class="col-sm-4" >
      <div class="btn-group">
-  <button type="button" class="btn btn-primary">Apple</button>
-  <button type="button" class="btn btn-primary">Samsung</button>
-  <button type="button" class="btn btn-primary">Sony</button>
+  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-filter"></span>FILTERS</button>
+  <button type="button" class="btn btn-default">GROUP BY</button>
+  <button type="button" class="btn btn-default">FAVOURITES</button>
 </div> 
     </div>
+</div>
+<br>
+<br>
+<div class="row" >
+    <div class="col-lg-3"  style="margin-left:20px;" >
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <img src="<?= base_url();?>public/assets/img/faces/marc.jpg" height="70px;" width="75px;" >
+                <b>Rajesh Kumar</b><br>
+                Address: Kamla Nagar<br>
+               <span class="glyphicon glyphicon-envelope"></span>&nbsp; Email: Rajesh@gmail.com<br>
+                <button type="button" class="btn btn-default">Following</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3"  style="margin-left:20px;" >
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <img src="<?= base_url();?>public/assets/img/faces/marc.jpg" height="70px;" width="75px;" >
+                <b>Rajesh Kumar</b><br>
+                Address: Kamla Nagar<br>
+                Email: Rajesh@gmail.com<br>
+                 <button type="button" class="btn btn-default">Following</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3"  style="margin-left:20px;" >
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <img src="<?= base_url();?>public/assets/img/faces/marc.jpg" height="70px;" width="75px;" >
+                <b>Rajesh Kumar</b><br>
+                Address: Kamla Nagar<br>
+                 <button type="button" class="btn btn-default">Following</button>
+            </div>
+        </div>
+    </div>
+            
 </div>
 <script src="<?= base_url()?>public/assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
 <script src="<?= base_url()?>public//assets/js/bootstrap.min.js" type="text/javascript"></script>
