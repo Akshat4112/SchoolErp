20e7932b8f38077642d1f09fed57032f821a866e
Akshat Gupta <akshat41121995@gmail.com>
2017-07-04 01:55:03 +0530

    Validation using model is created

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -6,16 +6,19 @@
  * Time: 12:49 AM
  */
 
-class Login_model extends MY_Model{
-    public function login_valid($email,$password){
-        $q = $this->db->where(['admin_email'=>$email,'admin_password'=>$password])
+class Login_model extends MY_Model
+{
+    public function login_valid($email, $password)
+    {
+        $q = $this->db->where(['admin_email' => $email, 'admin_password' => $password])
             ->get('admin');
 
-        if( $q->num_rows() >= 1){
+        if ($q->num_rows() >= 1) {
 
             return $q->row()->admin_id;
 
-        }else{
+        } else {
             return FALSE;
         }
     }
+}
