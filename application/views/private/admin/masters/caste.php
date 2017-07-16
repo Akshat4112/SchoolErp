7f93d903d5f519d28bc1cbd55e05024c353213e3
Akshat <akshat41121995@gmail.com>
2017-07-16 19:44:52 +0530

    maters modification
    caste, category and scetion UI added

--- application/views/private/admin/masters/caste.php
+++ application/views/private/admin/masters/caste.php
@@ -5,4 +5,68 @@
  * Date: 7/16/2017
  * Time: 1:04 PM
  */?>
-<h4>Caste of masters</h4>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akshat
+ * Date: 7/16/2017
+ * Time: 1:04 PM
+ */?>
+<div class="container">
+    <div class="row">
+        <h4>Caste of masters</h4>
+        <div class="col-lg-4">
+            <?php echo form_open('admmin/', ['class' => 'form-horizontal']); ?>
+            <div class="form-group">
+                <label for="inputText" class="col-lg-2 control-label">Caste</label>
+                <div class="col-lg-10">
+                    <?php echo form_input(['name' => 'caste', 'class' => 'form-control',
+                        'placeholder' => 'Enter caste',
+                        'value' => set_value('caste')]);
+                    ?>
+                    <?php echo form_error('caste'); ?>
+                </div>
+            </div>
+            <?php echo form_submit(['name' => 'submit', 'value' => 'Save', 'class' => 'btn btn-info',
+                'style' => 'margin-left:45px; margin-top:20px;']),
+            form_reset(['name' => 'reset', 'value' => 'reset', 'class' => 'btn btn-warning',
+                'style' => 'margin-top:20px;']); ?>
+
+
+        </div>
+        <div class="col-lg-4">
+            <table class="table table-striped table-hover ">
+                <thead>
+                <tr>
+                    <th>Caste</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>A</td>
+                </tr>
+                <tr>
+                    <td>B</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <?php echo form_open('admissions/', ['class' => 'form-horizontal']); ?>
+            <div class="form-group">
+                <div class="col-lg-12">
+                    <?php echo form_input(['name' => 'caste_del', 'class' => 'form-control',
+                        'placeholder' => 'Enter Caste to be deleted',
+                        'value' => set_value('caste_del')]);
+                    ?>
+                    <?php echo form_error('caste_del');
+                    echo '<br>';
+                    ?>
+                    <button name="subm" class="btn btn-danger">DELETE</button>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
--- application/views/private/admin/masters/category.php
+++ application/views/private/admin/masters/category.php
@@ -4,6 +4,62 @@
  * User: Akshat
  * Date: 7/16/2017
  * Time: 1:04 PM
- */
-?>
-<h4>category of masters</h4>
+ */?>
+<div class="container">
+    <div class="row">
+        <h4>Category of masters</h4>
+        <div class="col-lg-4">
+            <?php echo form_open('admmin/', ['class' => 'form-horizontal']); ?>
+            <div class="form-group">
+                <label for="inputText" class="col-lg-2 control-label">Category</label>
+                <div class="col-lg-10">
+                    <?php echo form_input(['name' => 'category', 'class' => 'form-control',
+                        'placeholder' => 'Enter Category',
+                        'value' => set_value('category')]);
+                    ?>
+                    <?php echo form_error('category'); ?>
+                </div>
+            </div>
+            <?php echo form_submit(['name' => 'submit', 'value' => 'Save', 'class' => 'btn btn-info',
+                'style' => 'margin-left:45px; margin-top:20px;']),
+            form_reset(['name' => 'reset', 'value' => 'reset', 'class' => 'btn btn-warning',
+                'style' => 'margin-top:20px;']); ?>
+
+
+        </div>
+        <div class="col-lg-4">
+            <table class="table table-striped table-hover ">
+                <thead>
+                <tr>
+                    <th>Category</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>A</td>
+                </tr>
+                <tr>
+                    <td>B</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <?php echo form_open('admissions/', ['class' => 'form-horizontal']); ?>
+            <div class="form-group">
+                <div class="col-lg-12">
+                    <?php echo form_input(['name' => 'category_del', 'class' => 'form-control',
+                        'placeholder' => 'Enter Category to be deleted',
+                        'value' => set_value('category_del')]);
+                    ?>
+                    <?php echo form_error('category_del');
+                    echo '<br>';
+                    ?>
+                    <button name="subm" class="btn btn-danger">DELETE</button>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
