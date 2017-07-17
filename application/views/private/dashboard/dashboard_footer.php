e2b0f021aed900af8d811d910f7f31654e92f040
Akshat <akshat41121995@gmail.com>
2017-07-17 23:26:25 +0530

    UI Changes

--- application/views/private/dashboard/dashboard_footer.php
+++ application/views/private/dashboard/dashboard_footer.php
@@ -1,27 +1,26 @@
-<footer class="footer">
-    <div class="container-fluid" style="margin-top: 80px;">
-        <nav class="pull-left">
+ <div class="container-fluid">
+        <nav>
             <ul>
                 <li>
                     <a href="<?= site_url('home/feedback')?>">
                         Feedback
                     </a>
-                </li>
+
                 <li>
                     <a href="<?= site_url('home/query')?>">
                         Query
                     </a>
-                </li>
+
                 <li>
                     <a href="<?= site_url('home/suggestions')?>">
                         Suggestions
                     </a>
-                </li>
+
                 <li>
                     <a href="<?= site_url('home/help')?>">
                         Help
                     </a>
-                </li>
+
             </ul>
         </nav>
         <p class="copyright pull-right">
@@ -30,10 +29,7 @@
             <a href="http://www.futureheadtech.com">Futurehead Technologies</a>
         </p>
     </div>
-</footer>
-</div>
-</div>
-</body>
+ </body>
 <!--   Core JS Files   -->
 <script src="<?= base_url() ?>public/assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
 <script src="<?= base_url() ?>public//assets/js/bootstrap.min.js" type="text/javascript"></script>
--- application/views/private/dashboard/dashboard_view.php
+++ application/views/private/dashboard/dashboard_view.php
@@ -1,52 +1,55 @@
 <?php include 'dashboard_header.php'; ?>
-    <div class="content">
-    <div class="container">
+    <div class="container" style="margin-left: 150px;">
         <div class="row">
             <div class="col-lg-4">
-                <a href="<?= site_url('admissions'); ?>"><img src="<?= base_url()?>/public/assets/img/admission.png" height="110" width="110"></a><br>
+                <a href="<?= site_url('admissions'); ?>"><img src="<?= base_url() ?>/public/assets/img/admission.png"
+                                                              height="110" width="110"></a><br>
                 <p style="font-size: 20px; margin-left: 26px;" class="text-info">Admissions</p>
             </div>
             <div class="col-lg-4">
-                <a href="<?= site_url('attendance/attend_view'); ?>"><img src="<?= base_url()?>/public/assets/img/attend.png" height="110" width="110"></a><br>
+                <a href="<?= site_url('attendance/attend_view'); ?>"><img
+                            src="<?= base_url() ?>/public/assets/img/attend.png" height="110" width="110"></a><br>
                 <p style="font-size: 20px; margin-left: 26px;" class="text-info">Attendance</p>
             </div>
             <div class="col-lg-4">
-                <a href="<?= site_url('admin/route_plan'); ?>"><img src="<?= base_url()?>/public/assets/img/transport.png" height="110" width="110"></a><br>
+                <a href="<?= site_url('admin/route_plan'); ?>"><img
+                            src="<?= base_url() ?>/public/assets/img/transport.png" height="110" width="110"></a><br>
                 <p style="font-size: 20px; margin-left: 26px;" class="text-info">Transport</p>
             </div>
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url()?>/public/assets/img/fees.png" height="110" width="110"></a><br>
-                        <p style="font-size: 20px; margin-left: 26px;" class="text-info">Fees</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url()?>/public/assets/img/library.png" height="110" width="110"></a><br>
-                        <p style="font-size: 20px; margin-left: 26px;" class="text-info">Library</p>
-                    </div>
-                    <div class="col-lg-4">
-                        <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url()?>/public/assets/img/tt.png" height="110" width="110"></a><br>
-                        <p style="font-size: 20px; margin-left: 26px;" class="text-info">Time Table</p>
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url()?>/public/assets/img/dues.png" height="110" width="110"></a><br>
-                                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Dues List</p>
-                            </div>
-                            <div class="col-lg-4">
-                                <a href="<?= site_url('summary/fees_summary'); ?>"><img src="<?= base_url()?>/public/assets/img/summary.png" height="110" width="110"></a><br>
-                                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Summary</p>
-                            </div>
-                            <div class="col-lg-4">
-                                <a href="<?= site_url('admin/'); ?>"><img src="<?= base_url()?>/public/assets/img/admin.png" height="110" width="110"></a><br>
-                                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Admin</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url() ?>/public/assets/img/fees.png"
+                                                                   height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Fees</p>
+            </div>
+            <div class="col-lg-4">
+                <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url() ?>/public/assets/img/library.png"
+                                                                   height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Library</p>
+            </div>
+            <div class="col-lg-4">
+                <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url() ?>/public/assets/img/tt.png"
+                                                                   height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Time Table</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="<?= site_url('admin/fees_plan'); ?>"><img src="<?= base_url() ?>/public/assets/img/dues.png"
+                                                                   height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Dues List</p>
+            </div>
+            <div class="col-lg-4">
+                <a href="<?= site_url('summary/fees_summary'); ?>"><img
+                            src="<?= base_url() ?>/public/assets/img/summary.png" height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Summary</p>
+            </div>
+            <div class="col-lg-4">
+                <a href="<?= site_url('admin/'); ?>"><img src="<?= base_url() ?>/public/assets/img/admin.png"
+                                                          height="110" width="110"></a><br>
+                <p style="font-size: 20px; margin-left: 26px;" class="text-info">Admin</p>
+            </div>
+        </div>
+    </div>
 <?php include 'dashboard_footer.php'; ?>
