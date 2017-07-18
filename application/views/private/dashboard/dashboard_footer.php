6121acc71229fc36a900504515aa57b4e46a51f1
Akshat <akshat41121995@gmail.com>
2017-07-18 11:16:24 +0530

    Footer UI changed

--- application/views/private/dashboard/dashboard_footer.php
+++ application/views/private/dashboard/dashboard_footer.php
@@ -1,34 +1,14 @@
- <div class="container-fluid">
-        <nav>
-            <ul>
-                <li>
-                    <a href="<?= site_url('home/feedback')?>">
-                        Feedback
-                    </a>
-
-                <li>
-                    <a href="<?= site_url('home/query')?>">
-                        Query
-                    </a>
-
-                <li>
-                    <a href="<?= site_url('home/suggestions')?>">
-                        Suggestions
-                    </a>
-
-                <li>
-                    <a href="<?= site_url('home/help')?>">
-                        Help
-                    </a>
-
-            </ul>
-        </nav>
-        <p class="copyright pull-right">
-            &copy;
-            <script>document.write(new Date().getFullYear())</script>
-            <a href="http://www.futureheadtech.com">Futurehead Technologies</a>
-        </p>
+<footer class="footer" style="margin-top: 27px;">
+    <div class="container">
+                <a href="<?= site_url('home/feedback')?>">Feedback |</a>
+                <a href="<?= site_url('home/query')?>">Query | </a>
+                <a href="<?= site_url('home/suggestions')?>">Suggestions |</a>
+                <a href="<?= site_url('home/help')?>">Help </a>
+        <p class="pull-right">Copyright 2017 at <a href="www.futureheadtech.com">Futurehead Technologies</a></p>
     </div>
+
+</footer>
+
  </body>
 <!--   Core JS Files   -->
 <script src="<?= base_url() ?>public/assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
