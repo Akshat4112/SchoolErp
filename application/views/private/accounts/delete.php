2241bb69ab239dc12ae9cdd2e7084a7a95f8a7a2
Akshat <akshat41121995@gmail.com>
2017-07-28 15:13:41 +0530

    Some Structural Changes

--- application/views/private/accounts/delete.php
+++ application/views/private/accounts/delete.php
@@ -9,7 +9,7 @@
     <div class="row">
         <p class="text-info" style="font-size: 20px;">Delete</p>
         <div class="col-lg-6">
-
+        
         </div>
         <div class="col-lg-6"></div>
     </div>
