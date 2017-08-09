f5732680b39fe85239bb7c5d442dc5e49c0277ce
Ridhima Garg <garg.ridhima72@gmail.com>
2017-08-09 15:49:55 +0530

    Fees Reports function declaration

--- application/controllers/fees_reports.php
+++ application/controllers/fees_reports.php
@@ -19,4 +19,19 @@ class Fees_reports extends MY_Controller {
     public function balance_fees_report(){
         $this->load->view('private/fees/fees_reports/balance_fees_report');
     }
+    public function fees_receipt_register(){}
+    public function fees_head_summary(){}
+    public function fees_monthly_summary(){}
+    public function fees_concc_settingsI(){}
+    public function defaulter_list(){}
+    public function yearly_receivable_report(){}
+    public function receipt_book(){}
+    public function fees_day_book(){}
+    public function head_wise_collection(){}
+    public function fees_group_ledger(){}
+    public function fees_structure(){}
+    public function fees_summary_date_wise(){}
+    public function fees_category_concc_wise(){}
+    public function fees_head_wise_concc_report(){}
+    public function concession_register(){}
 }
