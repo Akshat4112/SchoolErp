7e151d5d456d6382413a9d21fc5b14173d32b065
Akshat <akshat41121995@gmail.com>
2017-08-22 12:27:45 +0530

    UI Changes in defaulter list

--- application/views/private/fees/fees_reports/defaulter_list.php
+++ application/views/private/fees/fees_reports/defaulter_list.php
@@ -4,4 +4,15 @@
  * User: Akshat
  * Date: 8/9/2017
  * Time: 12:30 AM
- */
+ */?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="text-info" style="font-size: 20px">Defaulter List</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6"></div>
+        <div class="col-lg-6"></div>
+    </div>
+</div>
