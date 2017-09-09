86eb79e0df721f9b7b9e45998e241714df073a5f
Akshat <akshat41121995@gmail.com>
2017-09-09 20:45:31 +0530

    UX Improvement in Admission Create

--- application/views/private/admissions/create_admission_view.php
+++ application/views/private/admissions/create_admission_view.php
@@ -15,12 +15,12 @@
             </ul>
         </div>
     </div>
-    <div class="row">
+    <div class="row" style="margin-top: 10px;">
         <div class="col-md-5">
             <?php echo form_open_multipart('admissions/student_details', ['class' => 'form-horizontal']); ?>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">First Name</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">First Name</label>
+                <div class="col-lg-4">
                     <?php echo form_input(['name' => 'student_first_name', 'class' => 'form-control',
                         'placeholder' => 'Enter First Name',
                         'value' => set_value('student_first_name')]);
@@ -29,16 +29,16 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Middle Name</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Middle Name</label>
+                <div class="col-lg-5">
                     <?php echo form_input(['name' => 'student_middle_name', 'class' => 'form-control',
                         'placeholder' => 'Enter Middle Name',
                         'value' => set_value('student_middle_name')]); ?>
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Last Name</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Last Name</label>
+                <div class="col-lg-4">
 
                     <?php echo form_input(['name' => 'student_last_name', 'class' => 'form-control',
                         'placeholder' => 'Enter Last Name',
@@ -47,15 +47,15 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Date of Birth</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Date of Birth</label>
+                <div class="col-lg-5">
                     <input type="date" name="student_dob" class="form-control">
                     <?php echo form_error('student_dob'); ?>
                 </div>
             </div>
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">Gender</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">Gender</label>
+                <div class="col-lg-3">
                     <?php $options = [
                         'male' => 'Male',
                         'female' => 'Female',
@@ -68,13 +68,11 @@
                     echo form_dropdown('gender', $options,set_value('gender'), $attribute_class);
                     ?>
                     <?php echo form_error('gender'); ?>
-
-                    <br>
                 </div>
             </div>
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">Category</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">Category</label>
+                <div class="col-lg-4">
                     <?php
                     $drop = array();
                     foreach ($category_drop as $r) {
@@ -87,14 +85,11 @@
                     echo form_dropdown('category', $drop, '1', $attribute_class);
                     ?>
                     <?php echo form_error('category'); ?>
-                    <br>
                 </div>
             </div>
-        </div>
-        <div class="col-md-5">
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">Caste</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">Caste</label>
+                <div class="col-lg-4">
                     <?php
                     $drop = array();
                     foreach ($caste_drop as $r) {
@@ -107,12 +102,11 @@
                     echo form_dropdown('caste', $drop, '1', $attribute_class);
                     ?>
                     <?php echo form_error('caste'); ?>
-                    <br>
                 </div>
             </div>
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">Class</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">Class</label>
+                <div class="col-lg-3">
 
                     <?php
                     $drop = array();
@@ -128,9 +122,11 @@
                     <?php echo form_error('student_class'); ?>
                 </div>
             </div>
+        </div>
+        <div class="col-md-5">
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">Section</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">Section</label>
+                <div class="col-lg-2">
                     <?php
                     $drop = array();
                     foreach ($section_drop as $r) {
@@ -144,27 +140,27 @@
                     ?>
                     <?php echo form_error('student_section'); ?>
                 </div>
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Roll No.</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Roll No.</label>
+                <div class="col-lg-3">
                     <?php echo form_input(['name' => 'student_roll_no', 'class' => 'form-control',
-                        'placeholder' => 'Enter Roll no.',
+                        'placeholder' => 'Roll no.',
                         'value' => set_value('student_roll_no')]); ?>
                     <?php echo form_error('student_roll_no'); ?>
                 </div>
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Route</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Route</label>
+                <div class="col-lg-4">
                     <?php echo form_input(['name' => 'route', 'class' => 'form-control',
                         'placeholder' => 'Enter Route',
                         'value' => set_value('route')]); ?>
                 </div>
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="select" class="col-lg-2 control-label">House</label>
-                <div class="col-lg-10">
+                <label for="select" class="col-lg-3 control-label">House</label>
+                <div class="col-lg-4">
                     <?php
                     $drop = array();
                     foreach ($house_drop as $r) {
@@ -179,21 +175,20 @@
                     <?php echo form_error('house'); ?>
                     <br>
                 </div>
-            </div>
+            </div><br><br>
             <div class="form-group">
-                <label for="inputText" class="col-lg-2 control-label">Select Photo</label>
-                <div class="col-lg-10">
-                    <?php echo form_upload(['name' => 'userfile', 'class' => 'form-control',]);
+                <label for="inputText" class="col-lg-3 control-label">Select Photo</label>
+                <div class="col-lg-4">
+                    <?php echo form_upload(['name' => 'userfile', 'class' => 'form-control']);
                     ?>
                 </div>
-            </div>
+            </div><br>
             <?php  if(isset($upload_error)) echo $upload_error ?>
-            <br>
             <div class="form-group">
-                <b><p style="margin-left: 16px;" class="text-success">Last Admission
+                <b><p style="margin-left: 16px;" class="text-success"><br>Last Admission
                         Number: <?php echo $last_adm; ?></p></b>
-                <label for="inputText" class="col-lg-2 control-label">Admission No.</label>
-                <div class="col-lg-10">
+                <label for="inputText" class="col-lg-3 control-label">Admission No.</label>
+                <div class="col-lg-6">
                     <?php echo form_input(['name' => 'admission_no', 'class' => 'form-control',
                         'placeholder' => 'Enter Admission Number',
                         'value' => set_value('admission_no')]); ?>
