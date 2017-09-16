39e4eeffdde5a53304fd1253512978dcdaa7b70e
Akshat <akshat41121995@gmail.com>
2017-09-16 20:48:22 +0530

    UI changes in Attendance

--- application/controllers/attendance.php
+++ application/controllers/attendance.php
@@ -6,78 +6,81 @@
  * Time: 8:22 PM
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Attendance extends MY_Controller{
+
+class Attendance extends MY_Controller
+{
     public function __construct()
     {
         parent::__construct();
 
         // for checking if user is logged in or not.
 
-        if( ! $this->session->userdata('login_id')){
+        if (!$this->session->userdata('login_id')) {
             return redirect('home');
             exit();
         }
 
-        $this->load->view('private/attendance/header.php',['username'=>$this->get_admin()]);
+        $this->load->view('private/attendance/header.php', ['username' => $this->get_admin()]);
         $this->load->view('private/attendance/footer.php');
 
     }
-    public function attend_view(){
+
+    public function attend_view()
+    {
         $data = $this->input->post();
 
         $this->load->view('private/attendance/attend_view.php');
     }
 
-    public function summary(){
+    public function summary()
+    {
         $this->load->view('private/attendance/summary/summary.php');
     }
-    public function prints(){
+
+    public function prints()
+    {
         $this->load->view('private/attendance/print.php');
     }
-    public function attend_new(){
-        $this->load->model('get_model','gm');
+
+    public function attend_new()
+    {
+        $this->load->model('get_model', 'gm');
 
         //loading section dropdown form database
-        $section_list = $this->dropdown_db('section_name','section');
+        $section_list = $this->dropdown_db('section_name', 'section');
 
         //loading class dropdown from database
-        $class_list = $this->dropdown_db('class','class');
+        $class_list = $this->dropdown_db('class', 'class');
 
-        if($this->form_validation->run('attend_new'))
-        {
+        if ($this->form_validation->run('attend_new')) {
             $post = $this->input->post();
             unset($post['submit']);
 //            print_r($post);
 
-            $get_data = $this->gm->attendance_search($post['class'],$post['section']);
+            $get_data = $this->gm->attendance_search($post['class'], $post['section']);
 
 
-
-            $this->load->view('private/attendance/new/attend_new.php',['section_drop'=>$section_list,
-                'class_drop'=>$class_list,'data'=>$get_data]);
-        }
-        else{
-            $data=array();
-            $this->load->view('private/attendance/new/attend_new.php',['section_drop'=>$section_list,
-                'class_drop'=>$class_list,'data'=>$data]);
+            $this->load->view('private/attendance/new/attend_new.php', ['section_drop' => $section_list,
+                'class_drop' => $class_list, 'data' => $get_data]);
+        } else {
+            $data = array();
+            $this->load->view('private/attendance/new/attend_new.php', ['section_drop' => $section_list,
+                'class_drop' => $class_list, 'data' => $data]);
         }
 
     }
+
     public function attend_new_insert()
     {
-if($_POST)
-{
-    $field_array = array();
-    for( $i=0 ; $i < count($_POST['optradio']) ; $i++ )
-    {
-        $field_array[$i]['optradio']        = $_POST['optradio'][$i];
-        echo $field_array[$i]['optradio'];
-
-
-        // if you require then the query for your database
+        if ($_POST) {
+            $field_array = array();
+            for ($i = 0; $i < count($_POST['optradio']); $i++) {
+                $field_array[$i]['optradio'] = $_POST['optradio'][$i];
+                echo $field_array[$i]['optradio'];
+                // if you require then the query for your database
+            }
+//            $array = json_decode($data);
+//            print_r($array);
+        }
     }
-    $array = json_decode($data);
-    print_r($array);
-}
-}
 }
--- application/views/private/attendance/new/attend_new.php
+++ application/views/private/attendance/new/attend_new.php
@@ -6,132 +6,131 @@
  * Time: 11:30 AM
  */
 ?>
-    <div class="container">
-        <div class="row">
-            <p style="font-size: 20px; margin-top:0px;" class="text-default">Attendance / New</p>
+<div class="container">
+    <div class="row">
+        <p style="font-size: 20px; margin-top:0px;" class="text-default">Attendance / New</p>
+    </div>
+    <div class="row">
+        <div class="col-lg-3" style="margin-right:-10px;">
+            <?php echo form_open('attendance/attend_new', ['class' => 'form-horizontal']); ?>
+            <div class="form-group">
+                <label for="inputText" class="col-lg-2 control-label">Date</label>
+                <div class="col-lg-9">
+                    <input type="date" class="form-control" name="entry_date">
+                </div>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-lg-3" style="margin-right:-10px;">
-                <?php echo form_open('attendance/attend_new', ['class' => 'form-horizontal']); ?>
-                <div class="form-group">
-                    <label for="inputText" class="col-lg-2 control-label">Date</label>
-                    <div class="col-lg-9">
-                        <input type="date" class="form-control" name="entry_date">
-                    </div>
+        <div class="col-lg-2" style="margin-right: -30px;">
+            <div class="form-group">
+                <label for="inputText" class="col-lg-5 control-label" style="margin-top: 8px;">Class</label>
+                <div class="col-lg-7">
+                    <?php
+                    $drop = array();
+                    foreach ($class_drop as $r) {
+                        $drop[$r['class']] = $r['class'];
+                    }
+                    $attribute_class = [
+                        'class' => 'form-control',
+                        'id' => 'select',
+                    ];
+                    echo form_dropdown('class', $drop, set_value('class'), $attribute_class);
+                    ?>
+                    <?php echo form_error('class'); ?>
+
                 </div>
             </div>
-                <div class="col-lg-2" style="margin-right: -30px;">
-                    <div class="form-group">
-                        <label for="inputText" class="col-lg-5 control-label" style="margin-top: 8px;">Class</label>
-                        <div class="col-lg-7">
-                            <?php
-                            $drop = array();
-                            foreach ($class_drop as $r) {
-                                $drop[$r['class']] = $r['class'];
-                            }
-                            $attribute_class = [
-                                'class' => 'form-control',
-                                'id' => 'select',
-                            ];
-                            echo form_dropdown('class', $drop, set_value('class'), $attribute_class);
-                            ?>
-                            <?php echo form_error('class'); ?>
-
-                        </div>
-                    </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="form-group">
+                <label for="inputText" class="col-lg-4 control-label" style="margin-top: 8px;">Section</label>
+                <div class="col-lg-4">
+                    <?php
+                    $drop = array();
+                    foreach ($section_drop as $r) {
+                        $drop[$r['section_name']] = $r['section_name'];
+                    }
+                    $attribute_class = [
+                        'class' => 'form-control',
+                        'id' => 'select',
+                    ];
+                    echo form_dropdown('section', $drop, set_value('section'), $attribute_class);
+                    ?>
+                    <?php echo form_error('section'); ?>
                 </div>
-                <div class="col-lg-3">
-                    <div class="form-group">
-                        <label for="inputText" class="col-lg-4 control-label" style="margin-top: 8px;">Section</label>
-                        <div class="col-lg-4">
-                            <?php
-                            $drop = array();
-                            foreach ($section_drop as $r) {
-                                $drop[$r['section_name']] = $r['section_name'];
-                            }
-                            $attribute_class = [
-                                'class' => 'form-control',
-                                'id' => 'select',
-                            ];
-                            echo form_dropdown('section', $drop, set_value('section'), $attribute_class);
-                            ?>
-                            <?php echo form_error('section'); ?>
-                        </div>
-                        <?php echo form_submit(['name' => 'submit', 'value' => 'Search', 'class' => 'btn btn-primary btn-sm',
-                            'style' => 'margin-top:4px; margin-left:10px;']); ?>
-                    </div>
-                </div>
-        </div><?php echo form_close(); ?>
-         <?php echo form_open('attendance/attend_new_insert', ['class' => 'form-horizontal']);?>
-        <div class="col-lg-8">
-            <table class="table table-hover table-bordered">
-                <thead>
-                <tr class="text-info">
-                    <th>Class</th>
-                    <th>Section</th>
-                    <th>Student Id</th>
-                    <th>Roll No.</th>
-                    <th>Name</th>
-                    <th>Admission No.</th>
-                    <th>Present</th>
-                    <th>Absent</th>
-                    <th>Leave</th>
-                    <th>Remark</th>
-                </tr>
-                </thead>
-                <tbody>
-               
-                <?php if (count($data)): ?>
-             <?php foreach ($data as $student_det): ?>
-                        <tr class="">
-                            <td><?php echo $student_det['student_class'];?></td>
-                            <?php $myArray = array();
-                            $myArray = $student_det['student_class'];?>
-                            <td><?php echo $student_det['student_section'];?></td>
-                            <td><?php echo $student_det['student_id'];?></td>
-                            <td><?php echo $student_det['student_roll_no'];?></td>
-                            <td><?php echo $student_det['student_first_name'] ?></td>
-                            <td><?php echo $student_det['admission_no'] ?></td>
-                            <?php $data = json_encode($myArray); ?>
-
-
-                            <td>
-                                <?php $data = array('name'=>'optradio[]', 'id'=>'P','value'=>'Present', 'checked'=>'True');
-                                      echo form_checkbox($data);
-                                ?>
-                            </td>
-                            <td>
-                                <?php $data = array('name'=>'optradio[]', 'id'=>'A','value'=>'Absent');
-                                      echo form_checkbox($data);?>
-                                      <script>
-                                      if(document.getElementByID("P").checked)
-                                      {
-                                        alert("ajsj");
-                                        document.getElementByID('P').checked=false;
-                                      }
-                                        </script>
-                                        <?php
-                                ?>
-                            </td>
-                            <td>
-                                <?php $data = array('name'=>'optradio[]', 'id'=>'L','value'=>'');
-                                      echo form_checkbox($data);
-                                ?>
-                            </td>
-                            <td><?php echo ''; ?></td>
-                        </tr>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <tr class="">
-                            <td>No Records Found</td>
-                        </tr>
-                    <?php endif; ?>
-
-                    </tbody>
-                </table>
+                <?php echo form_submit(['name' => 'submit', 'value' => 'Search', 'class' => 'btn btn-primary btn-sm',
+                    'style' => 'margin-top:4px; margin-left:10px;']); ?>
             </div>
-                                <?php echo form_submit(['name' => 'insert', 'value' => 'Save', 'class' => 'btn btn-primary btn-sm',
-                            'style' => 'margin-top:4px; margin-left:10px;']); ?>
-                    <?php echo form_close();?>
         </div>
     </div>
+    <?php echo form_close(); ?>
+    <?php echo form_open('attendance/attend_new_insert', ['class' => 'form-horizontal']); ?>
+    <div class="row">
+    <div class="col-lg-8" style="overflow-y:scroll; overflow-x:hidden; height: 350px; width:850px;">
+        <table class="table table-hover table-bordered">
+            <thead>
+            <tr class="text-default">
+                <th>Class</th>
+                <th>Section</th>
+                <th>Student Id</th>
+                <th>Roll No.</th>
+                <th>Name</th>
+                <th>Admission No.</th>
+                <th>Present</th>
+                <th>Absent</th>
+                <th>Leave</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if (count($data)): ?>
+                <?php foreach ($data as $student_det): ?>
+                    <tr class="">
+                        <td>
+                            <?php echo $student_det['student_class']; ?>
+                        </td>
+                        <?php $myArray = array();
+                        $myArray = $student_det['student_class']; ?>
+                        <td><?php echo $student_det['student_section']; ?></td>
+                        <td><?php echo $student_det['student_id']; ?></td>
+                        <td><?php echo $student_det['student_roll_no']; ?></td>
+                        <td><?php echo $student_det['student_first_name'] ?></td>
+                        <td><?php echo $student_det['admission_no'] ?></td>
+                        <td>
+                            <?php $data = array('name' => 'optradio[]', 'id' => 'P', 'value' => 'Present', 'checked' => 'True');
+                            echo form_checkbox($data);
+                            ?>
+                        </td>
+                        <td>
+                            <?php $data = array('name' => 'optradio[]', 'id' => 'A', 'value' => 'Absent');
+                            echo form_checkbox($data); ?>
+                            <script>
+                                if (document.getElementByID("P").checked) {
+                                    alert("ajsj");
+                                    document.getElementByID('P').checked = false;
+                                }
+                            </script>
+                            <?php
+                            ?>
+                        </td>
+                        <td>
+                            <?php $data = array('name' => 'optradio[]', 'id' => 'L', 'value' => '');
+                            echo form_checkbox($data);
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr class="">
+                    <td>No Records Found</td>
+                </tr>
+            <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+    <div class="row">
+        <?php echo form_submit(['name' => 'insert', 'value' => 'Save', 'class' => 'btn btn-primary btn-sm',
+            'style' => 'margin-top:25px; margin-left:40px;']); ?>
+        <?php echo form_close(); ?>
+    </div>
+</div>
+
