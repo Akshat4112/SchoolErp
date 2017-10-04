a2a85f172c5ad9edfc8e5c65eff2eb6e30aa00bf
Akshat <akshat41121995@gmail.com>
2017-10-05 00:56:11 +0530

    Again Object to Array

--- application/models/student_model.php
+++ application/models/student_model.php
@@ -13,34 +13,34 @@ class Student_model extends MY_Model {
     }
     public function order_by_one($field_name,$by,$table_name){
         $query = $this->db->order_by($field_name,$by)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
     public function order_by_two($field_name,$by,$table_name){
         $query = $this->db->order_by($field_name,$by)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
     public function order_by_three($field_name,$by,$table_name){
         $query = $this->db->order_by($field_name,$by)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
     public function search_one($table_name,$search_col_1,$sb1){
         $query = $this->db->select('*')->where($search_col_1,$sb1)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
     public function search_two($table_name,$search_col_1,$sb1,$search_col_2,$sb2){
         $query = $this->db->select('*')->where($search_col_1,$sb1)->where($search_col_2
         ,$sb2)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
     public function search_three($table_name,$search_col_1,$sb1,$search_col_2,$sb2,$search_col_3,$sb3){
         $query = $this->db->select('*')->where($search_col_1,$sb1)->where($search_col_2
             ,$sb2)->where($search_col_3,$sb3)->get($table_name);
-        $res = $query->result();
+        $res = $query->result_array();
         return $res;
     }
 }
