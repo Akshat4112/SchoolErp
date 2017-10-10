2cd4413d5ec249b033f2ab50db0b17b482677b78
Ridhima <garg.ridhima72@gmail.com>
2017-10-10 09:01:57 +0530

    fees hogya plan !

---
+++ application/models/insert_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Insert_model extends MY_Model
+{
+	public function insert_fees_plan($data)
+	{
+		
+
+		foreach ($data['category_name'] as $category ) {
+			foreach ($data['class'] as $class) {
+				
+		$this->db->insert('fees_plan',array('fees_heading'=>$data['fees_heading'],'value'=>$data['fees_value'],'category'=>$category,'class'=>$class));
+	}
+}
+	}
+
+}
+
+?>
