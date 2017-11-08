5fd551a2efaa0e6fabba2daea14140a13756c665
Akshat <akshat41121995@gmail.com>
2017-11-09 00:05:14 +0530

    UI of SMS Template

--- application/views/private/admissions/send_sms.php
+++ application/views/private/admissions/send_sms.php
@@ -5,4 +5,18 @@
         </div>
         <div class="col-lg-6"></div>
     </div>
+    <div class="row">
+        <div class="col-lg-5">
+            <textarea rows="6" cols="40" class="form-control"></textarea>
+            <input type="submit" class="btn btn-sm btn-primary" value="Send" style="margin-top: 10px; margin-left: 10px;">
+        </div>
+        <div class="col-lg-3">
+            <button class="btn btn-success">Homework</button>
+            <button class="btn btn-success">Circulars</button>
+            <button class="btn btn-success" style="margin-top: 5px;">Define a new SMS Template</button>
+            <button class="btn btn-success" style="margin-top: 5px;">Select SMS Template to Send</button>
+        </div>
+        <div class="col-lg-2"></div>
+        <div class="col-lg-2"></div>
+    </div>
 </div>
